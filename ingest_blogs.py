fabbd2dda734652bcc7036f59e7217449ad43810
cruzer <swyam7@gmail.com>
2026-05-20 12:18:32 +0530

    Switch to local self-hosted Weaviate with Google vectorizer module

--- ingest_blogs.py
+++ ingest_blogs.py
@@ -47,7 +47,7 @@ with cm.connect_to_client() as client:
             wvc.config.Property(name="link", data_type=wvc.config.DataType.TEXT),
             wvc.config.Property(name="content", data_type=wvc.config.DataType.TEXT),
         ],
-        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_weaviate(),
+        vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_google(),
     )
     print(f"✅ Collection created.")
 
